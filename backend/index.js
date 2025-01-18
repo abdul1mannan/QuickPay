e78b582164903a7fee9552687357d15d531cc215
Abdul Mannan <mannan.jnv123@gmail.com>
2025-01-18 19:07:29 +0530

    Package.json

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // Route requests to /api/v1
 app.use('/api/v1', apiRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
